c14640bec235788300320c0e825f0bfe02c33032
rorfan8782 <cormacdev@gmail.com>
2017-05-16 23:20:28 +0100

    PO Info layout

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -8,4 +8,28 @@ MainCtrl.$inject = [];
 
 function MainCtrl() {
   var ctrl = this;
+
+  ctrl.receiptDateOpened;
+  ctrl.receivedValue;
+  ctrl.difference;
+  ctrl.dateFormat = 'MM/dd/yyyy';
+  ctrl.dateOptions = {
+    maxDate: new Date(2050, 12, 31),
+    minDate: new Date(2000, 1, 1),
+    startingDay: 1,
+    showWeeks: false
+  };
+  ctrl.altDateFormats = ['M!/d!/yyyy'];
+
+  ctrl.openReceiptDate = function() {
+    ctrl.receiptDateOpened = true;
+  }
+
+  function init() {
+    ctrl.receiptDateOpened = false;
+    ctrl.receivedValue = 0;
+    ctrl.difference = 0;
+  }
+
+  init();
 }
